4aefcc017e5d6572995e7ba52de6e0ae6735ce62
Ray Robertson <ray760@yahoo.com>
2020-11-24 14:20:33 -0500

    Updated mock page three.
    Updated url to point directly to json file.

---
+++ js/cards.js
@@ -0,0 +1,28 @@
+// Stored variables
+const parentEl = document.querySelector('.grid-row');
+
+// JSON-Server code
+
+const cardData = async () => {
+  let uri = 'http://localhost:3000/cards';
+  let local_uri = '../data-base/ds.json';
+
+  const response = await fetch(uri);
+  const cards = await response.json();
+
+  let cardTemplate = '';
+
+  cards.forEach( card => {
+    cardTemplate += `
+    <div class="desktop:grid-col-3 tablet:grid-col-4 mobile-lg:grid-col-6 grid-col-12">
+      <h5>${card.title}</h5>
+      <p>${card.data}</p>
+    </div>
+    `
+
+  });
+
+  parentEl.innerHTML = cardTemplate;
+}
+
+window.addEventListener('DOMContentLoaded', () => cardData());
